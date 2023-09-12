847576f53b7c29f02f6fcca5030b41b49477a503
sagar singh rauthan <82528533+sagar779@users.noreply.github.com>
2023-09-12 11:05:40 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+cv = pickle.load(open("model/cv.pkl", "rb"))
+clf = pickle.load(open("model/clf.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+        email = request.form.get("content")
+        tokenized_email =cv.transform([email])
+        prediction =clf.predict(tokenized_email) 
+        prediction = 1 if prediction ==1 else -1
+        return render_template("index.html", prediction=prediction, email=email)
+
+if __name__=="__main__":
+    app.run(debug=True)
